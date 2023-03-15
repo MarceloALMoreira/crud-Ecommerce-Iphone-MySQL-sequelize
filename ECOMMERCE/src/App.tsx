fa4bd923d777548be592a8974f7d03157251e228
marcelo.almeida <90574339+MarceloALMoreira@users.noreply.github.com>
2023-03-15 09:51:44 -0300

    feat create projeto

---
+++ ECOMMERCE/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h2>Bem vindo ao ecommerce</h2>
+    </>
+  )
+}
+
+export default App
