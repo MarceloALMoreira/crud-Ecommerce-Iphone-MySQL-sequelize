7e99e8e48b3898e89c8ac608afc335c1d5a4fcc1
marcelo.almeida <90574339+MarceloALMoreira@users.noreply.github.com>
2023-03-15 10:04:44 -0300

    criando a home

---
+++ ECOMMERCE/src/components/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <>
+            <div className="">
+                <p> Minha Home</p>
+            </div>
+
+        </>
+    )
+}
+
+export default Home
