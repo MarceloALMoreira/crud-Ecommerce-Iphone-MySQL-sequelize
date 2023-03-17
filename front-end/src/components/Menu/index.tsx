8f1a0af4479122d7b3d12b1a15009dd85e6d2f9f
marcelo.almeida <90574339+MarceloALMoreira@users.noreply.github.com>
2023-03-16 22:29:02 -0300

    feat criando funcções js

--- front-end/src/components/Menu/index.tsx
+++ front-end/src/components/Menu/index.tsx
@@ -1,8 +1,19 @@
+import logo from '../../assets/iphone/logo.png'
+import styles from './Menu.module.scss'
 const Menu = () => {
     return (
-        <div>
-            <p>Menu Superior</p>
-        </div>
+        <div className={styles.menu}>
+            <img className={styles.logo} src={logo} alt="" />
+            <ul>
+                <li>Mac</li>
+                <li>iPhone</li>
+                <li>iPad</li>
+                <li>Watch</li>
+                <li>AirPods</li>
+                <li>Acessórios</li>
+                <li>Suporte</li>
+            </ul>
+        </div >
     )
 }
 
--- front-end/src/components/iPhone/index.tsx
+++ front-end/src/components/iPhone/index.tsx
@@ -1,9 +1,13 @@
 import styles from './iPhone.module.scss'
 import phone from '../../assets/iphone/iphone-azul.png'
-const Phone = () => {
+import { useState } from 'react'
+
+function Phone() {
 
     return (
         <div className={styles.caixa_azul}>
+
+
 
             <div className="">
                 <h1 className={styles.title_iphone}>iPhone 14</h1>
@@ -14,21 +18,22 @@ const Phone = () => {
                 <h3 className={styles.preco}>A partir de R$ 633,25/mês ou R$ 7.599</h3>
 
                 <button className={styles.botao}>Comprar</button>
-               
+
                 <p className={styles.select_color}>Escolha sua cor:</p>
-              
+
                 <div className=''>
                     <button className={`${styles.update_color} ${styles.azul}`}></button>
                     <button className={`${styles.update_color} ${styles.amarelo}`}></button>
                     <button className={`${styles.update_color} ${styles.branco}`}></button>
                     <button className={`${styles.update_color} ${styles.preto}`}></button>
                     <button className={`${styles.update_color} ${styles.vermelho}`}></button>
-                    <button className={`${styles.update_color} ${styles.roxo}`}></button>                    
+                    <button className={`${styles.update_color} ${styles.roxo}`}></button>
                 </div>
             </div>
-
+            <div className={styles.circle}></div>
             <img src={phone} alt="" />
-        </div>
+        </div >
     )
 }
+
 export default Phone
