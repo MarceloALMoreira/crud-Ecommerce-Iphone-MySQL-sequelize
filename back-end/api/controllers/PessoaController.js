c79c89be2c022c1e82b0ddd5031bd3a57d137b8f
marcelo.almeida <90574339+MarceloALMoreira@users.noreply.github.com>
2023-04-17 10:59:10 -0300

    recuperando o id do exlucido temporario

--- back-end/api/controllers/PessoaController.js
+++ back-end/api/controllers/PessoaController.js
@@ -61,6 +61,16 @@ class PessoaController {
         }
     }
 
+    static async restaurapessoa(req, res) {
+        const { id } = req.params
+        try {
+            await database.Pessoas.restore({ where: { id: Number(id) } })
+            return res.status(200).json({ message: `id ${id} restaurado com sucesso` })
+
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
 
     // Metedos para  Matriculas
 
@@ -104,6 +114,19 @@ class PessoaController {
             return res.status(200).json({ message: `Matricula ${matriculaId} Deletado com sucesso!` })
         } catch (error) {
             return res.status(500).json(error.message)
+        }
+    }
+    static async restauraMatricula(req, res) {
+        const { estudanteId, matriculaId } = req.params
+        try {
+            await database.Matricula.restore({ where: { id: Number(matriculaId), estudante_id: Number(estudanteId) } })
+            return res.status(200).json({ message: `id ${id} restaurado com sucesso` })
+
+
+
+        } catch (error) {
+            return res.status(500).json(error.message)
+
         }
     }
 }
--- back-end/api/routes/pessoasRoute.js
+++ back-end/api/routes/pessoasRoute.js
@@ -9,11 +9,14 @@ router.get('/pessoas/:id', PessoaController.getPessoaId)
 router.post('/pessoas', PessoaController.createPessoa)
 router.put('/pessoas/:id', PessoaController.upPessoas)
 router.delete('/pessoas/:id', PessoaController.delPessoalId)
+router.post('/pessoas/:id/restaura', PessoaController.restaurapessoa) // pessoas/1/restaura || essa route vai restuarar uma pessoa
+
 
 router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.getMatriculaId)
 router.post('/pessoas/:estudanteId/matricula', PessoaController.createMatricula)
 router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.upMatricula)
 router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.delMatriculaId)
+router.post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
 
 
 
